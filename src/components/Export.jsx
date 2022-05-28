f7a5c6aa8e8025c8218ecc809283e68f876296e4
German <gaordonezh@gmail.com>
2022-05-28 10:50:43 -0500

    Remove async function

--- src/components/Export.jsx
+++ src/components/Export.jsx
@@ -1,28 +1,16 @@
-import React, { useState } from "react";
 import html2Canvas from "html2canvas";
-import sleep from "../utils/sleep";
 
 const Export = ({ name }) => {
-  const [loading, setLoading] = useState(false);
-
   const exportar = async () => {
-    setLoading(true);
     const canvas = await html2Canvas(document.querySelector("#meme"));
     const img = canvas.toDataURL("image/png");
     const link = document.createElement("a");
     link.download = name;
     link.href = img;
     link.click();
-
-    await sleep(1000);
-    setLoading(false);
   };
 
-  return (
-    <button onClick={exportar} disabled={loading}>
-      {loading ? "CARGANDO..." : "EXPORTAR"}
-    </button>
-  );
+  return <button onClick={exportar}>EXPORTAR</button>;
 };
 
 export default Export;
