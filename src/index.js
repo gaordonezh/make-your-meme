76f52c5089fce012f63f1613145feddf3fae6673
German <gaordonezh@gmail.com>
2022-05-27 23:18:13 -0500

    FIXED PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,4 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.unregister();
+reportWebVitals();
+
+serviceWorkerRegistration.register();
